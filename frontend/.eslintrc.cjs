048b2a2bfcaac479e377d6bcc3a3dc47eeb378fd
Mohamed Ismail <ismailreality1@gmail.com>
2023-09-21 17:08:54 +0200

    chore: use eslint react plugin

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,12 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
