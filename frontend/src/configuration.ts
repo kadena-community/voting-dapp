49be4504b73963f775e9d2fee77f5d6fbd518fdd
Mohamed Ismail <ismailreality1@gmail.com>
2023-09-21 11:01:50 +0200

    chore: remove react-scripts and use vite

--- frontend/src/configuration.ts
+++ frontend/src/configuration.ts
@@ -3,5 +3,5 @@ export type Backend = "in-memory" | "devnet" | "testnet";
 const defaultBackend: Backend = "devnet";
 
 export function getBackend(): Backend {
-  return (process.env.REACT_APP_BACKEND as Backend) || defaultBackend;
+  return (import.meta.env.VITE_APP_BACKEND as Backend) || defaultBackend;
 }
