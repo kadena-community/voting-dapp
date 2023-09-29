0dc50511cc4e0ae277b248bceaa0b2cc5743b361
Jesse van Muijden <jessevanmuijden@gmail.com>
2023-09-28 22:44:54 -0400

    feat(tutorial): add snippet, pact and repl files for chapter 04

---
+++ snippets/principal-namespace.ts
@@ -0,0 +1,42 @@
+import { Pact, createClient, isSignedTransaction, signWithChainweaver } from '@kadena/client';
+import { getApiHost, getChainId, getNetworkId } from './configuration';
+
+const client = createClient(getApiHost());
+
+if (! process.argv[2]) {
+  console.error('Please specify a Kadena account.');
+}
+
+const accountKey = (account: string): string => account.split(':')[1];
+
+main(process.argv[2]);
+
+async function main(account: string) {
+  const pactCommand = `
+    (let ((ns-name (ns.create-principal-namespace (read-keyset 'admin-keyset))))
+      (define-namespace ns-name (read-keyset 'admin-keyset ) (read-keyset 'admin-keyset ))
+    )
+  `
+  const transaction = Pact.builder
+    .execution(pactCommand)
+    .addData('admin-keyset', {
+      keys: [accountKey(account)],
+      pred: 'keys-all',
+    })
+    .addSigner(accountKey(account))
+    .setMeta({ chainId: getChainId(), senderAccount: account })
+    .setNetworkId(getNetworkId())
+    .createTransaction();
+
+  const signedTx = await signWithChainweaver(transaction);
+
+  if (isSignedTransaction(signedTx)) {
+    const transactionDescriptor = await client.submit(signedTx);
+    const response = await client.listen(transactionDescriptor);
+    if (response.result.status === 'failure') {
+      throw response.result.error;
+    } else {
+      console.log(response.result);
+    }
+  }
+}
