e683c72b719ca6c2e8c982c260cc6093dc5d410d
Lisa Gunn <lisa@kadena.io>
2025-05-16 15:31:35 -0700

    Election workshop work to save

--- frontend/src/repositories/candidate/DevnetCandidateRepository.ts
+++ frontend/src/repositories/candidate/DevnetCandidateRepository.ts
@@ -1,16 +1,22 @@
-import { Pact, createClient, isSignedTransaction, signWithChainweaver } from '@kadena/client';
-import { ICandidate } from '../../types';
+import { Pact, createClient, isSignedTransaction, createSignWithKeypair } from '@kadena/client';
+import { IKeyPair } from '@kadena/client';
+import { ICandidate } from '../../types.js';
 
 const NETWORK_ID = 'development';
-const CHAIN_ID = '1';
+const CHAIN_ID = '3';
 const API_HOST = `http://localhost:8080/chainweb/0.0/${NETWORK_ID}/chain/${CHAIN_ID}/pact`;
-const NAMESPACE = 'n_fd020525c953aa002f20fb81a920982b175cdf1a';
+const NAMESPACE = 'n_d5ff15d933b83c1ef691dce3dabacfdfeaeade80';
 
-const client = createClient(API_HOST);
+const kadenaClient = createClient(API_HOST);
 const accountKey = (account: string) => account.split(':')[1];
 
+interface IAccount {
+  accountName: string;
+  publicKey: string;
+}
+
 const listCandidates = async (): Promise<ICandidate[]> => {
-  const transaction = Pact.builder
+  const readTransaction = Pact.builder
     .execution(Pact.modules[`${NAMESPACE}.election`]['list-candidates']())
     .setMeta({
       chainId: CHAIN_ID,
@@ -19,39 +25,47 @@ const listCandidates = async (): Promise<ICandidate[]> => {
     .setNetworkId(NETWORK_ID)
     .createTransaction();
 
-  const { result } = await client.dirtyRead(transaction);
+  const { result } = await kadenaClient.dirtyRead(readTransaction);
 
   return result.status === 'success' ? (result.data.valueOf() as ICandidate[]) : [];
 };
 
-const addCandidate = async (candidate: ICandidate, sender: string = ''): Promise<void> => {
-  const transaction = Pact.builder
+const addCandidate = async (candidate: ICandidate, sender: IAccount): Promise<void> => {
+  const unsignedTransaction = Pact.builder
     .execution(
       Pact.modules[`${NAMESPACE}.election`]['add-candidate'](candidate),
     )
-    .addData('admin-keyset', {
-      keys: [accountKey(sender)],
+    .addData('election-admin', {
+      keys: [sender.publicKey],
       pred: 'keys-all',
     })
-    .addSigner(accountKey(sender))
+    .addSigner(sender.publicKey)
     .setMeta({
       chainId: CHAIN_ID,
-      senderAccount: sender,
+      senderAccount: sender.accountName,
     })
     .setNetworkId(NETWORK_ID)
     .createTransaction();
 
-  const signedTx = await signWithChainweaver(transaction);
+    const electionKeyPair:IKeyPair = {
+      publicKey:
+        'd0aa32802596b8e31f7e35d1f4995524f11ed9c7683450b561e01fb3a36c18ae',
+      secretKey:
+        '35003210deab99bc9652d1f254b0489a318ed996544d6db1160c7e1b320e0c72',
+    };
 
-  const preflightResponse = await client.preflight(signedTx);
+    const signWithKeypair = createSignWithKeypair([electionKeyPair]);
+    const signedTx = await signWithKeypair(unsignedTransaction);
+
+    const preflightResponse = await kadenaClient.preflight(signedTx);
 
   if (preflightResponse.result.status === 'failure') {
     throw preflightResponse.result.error;
   }
 
   if (isSignedTransaction(signedTx)) {
-    const transactionDescriptor = await client.submit(signedTx);
-    const response = await client.listen(transactionDescriptor);
+    const transactionDescriptor = await kadenaClient.submit(signedTx);
+    const response = await kadenaClient.listen(transactionDescriptor);
     if (response.result.status === 'failure') {
       throw response.result.error;
     } else {
--- frontend/src/repositories/vote/DevnetVoteRepository.ts
+++ frontend/src/repositories/vote/DevnetVoteRepository.ts
@@ -1,20 +1,20 @@
-import { Pact, createClient, isSignedTransaction, signWithChainweaver } from '@kadena/client';
+import { Pact, createClient, isSignedTransaction, signTransactions } from '@kadena/client';
 
 const NETWORK_ID = 'development';
-const CHAIN_ID = '1';
+const CHAIN_ID = '3';
 const API_HOST = `http://localhost:8080/chainweb/0.0/${NETWORK_ID}/chain/${CHAIN_ID}/pact`;
-const NAMESPACE = 'n_fd020525c953aa002f20fb81a920982b175cdf1a';
+const NAMESPACE = 'n_d5ff15d933b83c1ef691dce3dabacfdfeaeade80';
 
-const client = createClient(API_HOST);
+const kadenaClient = createClient(API_HOST);
 const accountKey = (account: string) => account.split(':')[1];
 
 const hasAccountVoted = async (account: string): Promise<boolean> => {
-  const transaction = Pact.builder
+  const readTransaction = Pact.builder
     .execution(Pact.modules[`${NAMESPACE}.election`]['account-voted'](account))
     .setMeta({ chainId: CHAIN_ID })
     .setNetworkId(NETWORK_ID)
     .createTransaction();
-  const { result } = await client.dirtyRead(transaction);
+  const { result } = await kadenaClient.dirtyRead(readTransaction);
 
   if (result.status === 'success') {
     return result.data.valueOf() as boolean;
@@ -25,7 +25,7 @@ const hasAccountVoted = async (account: string): Promise<boolean> => {
 };
 
 const vote = async (account: string, candidateKey: string): Promise<void> => {
-  const transaction = Pact.builder
+  const unsignedTransaction = Pact.builder
     .execution(
       Pact.modules[`${NAMESPACE}.election`].vote(account, candidateKey),
     )
@@ -44,11 +44,11 @@ const vote = async (account: string, candidateKey: string): Promise<void> => {
     .setNetworkId(NETWORK_ID)
     .createTransaction();
 
-  const signedTx = await signWithChainweaver(transaction);
+  const signedTx = await signTransactions("https://chainweaver.kadena.network:9467/");
 
   if (isSignedTransaction(signedTx)) {
-    const transactionDescriptor = await client.submit(signedTx);
-    const { result } = await client.listen(transactionDescriptor);
+    const transactionDescriptor = await kadenaClient.submit(signedTx);
+    const { result } = await kadenaClient.listen(transactionDescriptor);
     if (result.status === 'failure') {
       throw result.error;
     } else {
